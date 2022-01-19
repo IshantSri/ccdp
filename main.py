05103e5d4bb58d392eeae4956eba6d932be6eca7
ISHANT <ishantsrivastava386@gmail.com>
2022-01-19 17:08:55 +0530

    in progress....

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 from flask import Flask,render_template,request,redirect
+from prediction_pre_process import PREDICTION
+from data_trans import data_transform
+
 from flask_cors import CORS,cross_origin
 import pickle
 import pandas as pd
 import numpy as np
+app = Flask(__name__)
 
-app=Flask(__name__)
-cors=CORS(app)
-model= pickle.load(open('ccdp.pkl','rb'))
-d= datagetter('data.csv')
-da= d.getdata()
+@app.route("/",methods = ['GET'])
+def home():
+    return render_template('index.html')
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    return render_template('index.html',companies=companies, car_models=car_models, years=year,fuel_types=fuel_type)
-
-
-@app.route('/predict',methods=['POST'])
-@cross_origin()
-def predict():
-    prediction=model.predict()
-    print(prediction)
-
-    return str(np.round(prediction,2))
+@app.route("/", methods = ["POST"])
+def choose_file():
+    data= request.files['data']
+    dp = './dp' + data.filename
+    data.save(dp)
+    return render_template('index.html')
 
 
 
-if __name__=='__main__':
-    app.run()
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ rough.py
@@ -0,0 +1,27 @@
+#from model_operation.model_building import train
+#a = train('data.csv')
+#a.training()
+import numpy as np
+import pandas as pd
+import pickle
+from data_trans.data_transform import data_transform
+from prediction_pre_process.PREDICTION import predict
+from data_loader.importing_raw_data import datagetter
+p = predict()
+
+m = pickle.load(open('ccdp.pkl','rb'))
+
+d = datagetter('data.csv')
+da= d.getdata()
+
+dt = data_transform()
+d = p.pred_val(da)
+d = dt.label(d)
+#print(d)
+prediction = m.predict_proba(d)
+#print(pd.DataFrame(prediction,columns=['repayment','repayment']))
+prediction = pd.DataFrame(prediction,columns=['repayment','default'])
+
+da['prob_default'] = np.round(prediction['default'],2)
+da['prob_repayment']=np.round(prediction['repayment'],2)
+dt.tocsv(da)
